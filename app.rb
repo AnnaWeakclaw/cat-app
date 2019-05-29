991130ef9077edb39d782a223ef601fa0edda91a
adrem7 <adrian.raisin@gmail.com>
2019-05-29 14:53:34 +0100

    Added cat image and used CSS (inline) to surround with border

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ get '/' do
   "Hello World"
 end
 
+get '/secret' do
+  "boomhiuguygadfbfdbv"
+end
+
 get '/cat' do
-  @random_name = ['Amigo','Oscar','Viking'].sample
-  erb(:index)
+  '<img src="http://bit.ly/1eze8aE" alt="My cat" style="border: dashed red" >'
+  # @random_name = ['Amigo','Oscar','Viking'].sample
+  # erb(:index)
 end
