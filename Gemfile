85ed709bc8544f487d39c7c32458f2a2ed330d1b
Anna <annas.wieclaw@gmail.com>
2019-05-29 22:15:20 +0100

    Test post and get methods and add capybara framework for testing web'

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'shotgun'
+gem 'selenium-webdriver'
+gem 'capybara'
+gem 'pry', '~> 0.12.2'
+gem 'chromedriver-helper'
--- app.rb
+++ app.rb
@@ -13,8 +13,12 @@ get '/random-cat' do
    erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/only-name-form' do
+  erb :cat_form
 end
