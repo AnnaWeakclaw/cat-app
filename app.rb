22793e5baed5d9189cb3c4d16c7ff49e10a8ad14
Anna <annas.wieclaw@gmail.com>
2019-05-29 15:03:35 +0100

    Moved the image and randomly select a name from the index file

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ get '/secret' do
 end
 
 get '/cat' do
-  '<img src="http://bit.ly/1eze8aE" alt="My cat" style="border: dashed red" >'
-  # @random_name = ['Amigo','Oscar','Viking'].sample
-  # erb(:index)
+  
+   erb(:index)
 end
